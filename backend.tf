da08456462f11c33d26f080800130f52e784335d
May-camp <nadihlaing776655@gmail.com>
2026-06-28 16:21:42 +0000

    fix: route s3 backend endpoints to localstack explicitly

--- backend.tf
+++ backend.tf
@@ -1,14 +1,7 @@
 terraform {
   backend "s3" {
-    bucket                      = "my-local-b"
-    key                         = "state"
-    region                      = "us-east-1"
-    endpoint                    = "http://localhost:4566"
-    sts_endpoint                = "http://localhost:4566"
-    iam_endpoint                = "http://localhost:4566"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    use_path_style              = true
+    bucket = "my-local-b"
+    key    = "state"
+    region = "us-east-1"
   }
 }
